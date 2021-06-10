0d67d34d0096e8fe04dd90713592ff83bc228524
Adrisiav <adrisiav@espol.edu.ec>
2021-06-10 10:40:27 -0500

    Adrian Conectado

---
+++ Grupo8POO/src/grupo8poo/Grupo8POO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo8poo;
+
+/**
+ *
+ * @author Ricardo Siavichay
+ */
+public class Grupo8POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
