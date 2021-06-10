c0a5e98c01a82c74d375746e35e6dd01fc677012
Adrisiav <adrisiav@espol.edu.ec>
2021-06-10 18:37:47 -0500

    Adrian Conectado

--- TallerGrupal/src/videoplus/Cliente.java
+++ TallerGrupal/src/videoplus/Cliente.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package videoplus;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  *
@@ -12,6 +14,7 @@ package videoplus;
 public class Cliente {
     private ArrayList<Video> listaReproduccion;
     private String nombre;
+    Scanner sc = new Scanner(System.in);
     
     public Cliente(String nombre){
         this.nombre= nombre;
@@ -25,6 +28,17 @@ public class Cliente {
         }
         return false;
     }
+    public void reproducir(){
+            String continuar="no";
+            do{
+                Video videoInicial = listaReproduccion.get(0);
+                videoInicial.mostrarInformacion();
+                videoInicial.setNumeroReproducciones(videoInicial.getNumeroReproducciones()+1); 
+                listaReproduccion.remove(0);
+                continuar = sc.nextLine();
+            }while(continuar.equals("si"));
+        }
+        
 }
     
-}
+
