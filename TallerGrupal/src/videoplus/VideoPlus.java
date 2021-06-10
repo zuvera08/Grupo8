86fde9184f5688d87ca87d5b2fe5f0ae049dca91
Adrisiav <85650605+Adrisiav@users.noreply.github.com>
2021-06-10 18:01:29 -0500

    Add files via upload

---
+++ TallerGrupal/src/videoplus/VideoPlus.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videoplus;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rocio
+ */
+public class VideoPlus {
+    
+    public static void main(String[] args){
+        /*ArrayList<Video> videos = new ArrayList<Video>();
+        videos.add(new Video("Cruela"));
+        videos.add(new Video("Lazo While"));
+        videos.add(new Video("El rey Leon"));
+        videos.add(new Video("Chapie"));
+        videos.add(new Video("Blanca Nieves"));
+        videos.add(new Video("Buscando a neno"));
+        
+        Cliente c = new Cliente("jose");
+        //agregue cinco peluclas a la lista de reproduccion
+        
+        //reproduzca las peliculas*/
+    }
+}
